fa21704b4cfd3036871a81a42efb5ddbf9483b6e
Deonte McCoy <deontemccoy19@gmail.com>
2022-09-07 09:37:48 -0500

    Create empty array, object constuctor, and function to append array

---
+++ main.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+
+    myLibrary.push(book);
+};
+
+function displayBook() {
+    
+};
